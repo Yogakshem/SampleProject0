faaa9f3edab594bdd1b53063fed83adfc27c789d
yogakshem <yogakshem721@gmail.com>
2019-06-11 09:27:27 +0530

    SampleProject0

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,14 @@ class HelloWorld
 	public static void main(String args[])
 	{
 		System.out.println("Hello World");
+		int x,y;
+		x=Integer.parseInt(args[0]);
+		y=Integer.parseInt(args[1]);
+		sum(x,y);
+
+	}
+	static void sum(int a,int b)
+	{
+		System.out.println("The sum is "+(a+b));
 	}
 }
