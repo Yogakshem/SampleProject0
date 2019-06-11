148941629bf890b3bf7d6af989e6423e9f42b835
yogakshem <yogakshem721@gmail.com>
2019-06-11 10:02:55 +0530

    SampleProject0

---
+++ Test1.java
@@ -0,0 +1,10 @@
+class Test1
+{
+     public static void main(String args[])
+     {
+          int a,b;
+          a=Integer.parseInt(args[0]);
+          b=Integer.parseInt(args[1]);
+          HelloWorld.sum(a, b);          
+     }
+}
